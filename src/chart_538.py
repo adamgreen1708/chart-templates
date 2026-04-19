fd3a35606bc7766061611923238a76d450669a51
adamgreen1708 <120283107+adamgreen1708@users.noreply.github.com>
2026-04-19 19:34:21 +0100

    Create chart_538.py

---
+++ src/chart_538.py
@@ -0,0 +1,14 @@
+def apply_538_template(ax, fig, *, vertical_gridlines=False):
+    """
+    Apply the locked 538 template styling rules to a matplotlib chart.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes
+        Chart axes to style.
+    fig : matplotlib.figure.Figure
+        Parent figure.
+    vertical_gridlines : bool, default False
+        Whether vertical gridlines should be shown.
+    """
+    raise NotImplementedError("538 template styling not implemented yet.")
