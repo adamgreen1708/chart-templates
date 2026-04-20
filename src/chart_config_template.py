8a3cf305791fcfe51dd19941a25c2ac820b4f45c
adamgreen1708 <120283107+adamgreen1708@users.noreply.github.com>
2026-04-20 20:24:37 +0100

    Create chart_config_template.py

---
+++ src/chart_config_template.py
@@ -0,0 +1,18 @@
+CHART_CONFIG = {
+    "data_file": "",
+    "data_format": "wide",
+    "chart_type": "line",
+
+    "title": "",
+    "subtitle": "",
+    "source_text": "",
+    "footer_left": "Adam Green | coffeetableviz",
+
+    "x_col": "",
+    "y_col": "",
+
+    "reference_lines": [],
+    "highlight_points": [],
+    "auto_end_labels": True,
+    "end_labels": [],
+}
