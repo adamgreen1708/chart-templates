c936edfc4a99564ccf8407db02cfe8e73ed0000b
adamgreen1708 <120283107+adamgreen1708@users.noreply.github.com>
2026-04-26 20:31:57 +0100

    Update chart_config.py

--- src/chart_config.py
+++ src/chart_config.py
@@ -7,7 +7,7 @@ CHART_CONFIG = {
     "chart_type": "line",
 
     "x_col": "Year",
-    "y_col": "	Rape or colza seed - Production (tonnes)",
+    "y_col": "Rape or colza seed - Production (tonnes)",
     "series_col": "Entity",
     "value_col": "Rape or colza seed - Production (tonnes)",
 
