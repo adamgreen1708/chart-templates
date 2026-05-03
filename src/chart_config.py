1b6e9d3bcd6d61faaabe64e5f1f247f1168ebf44
adamgreen1708 <120283107+adamgreen1708@users.noreply.github.com>
2026-05-03 14:15:44 +0100

    Update chart_config.py

--- src/chart_config.py
+++ src/chart_config.py
@@ -1,5 +1,5 @@
 CHART_CONFIG = {
-    "data_file": "data/forbes_most_valuable_sports_team.csv",
+    "data_file": "data/forbes_most_valuable_sports_team",
     "data_format": "wide",
     "chart_type": "bar",
 
